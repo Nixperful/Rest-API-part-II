04a6e8f658f18e6f25337312c7c01f5c8f6d8877
Nicolás Osorio <nicolas.osorio@mail.escuelaing.edu.co>
2018-03-01 18:35:41 -0500

    Commit De subida

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -100,7 +100,7 @@ function loadOrders() {
             list = response.data;
             document.getElementById('tableMenu').innerHTML = "";
             for (order in list) {
-                console.log(order);
+                console.log(order.tableNumber);
                 var ordersTable = document.getElementById('tableMenu');
                 var ord = document.createElement('table');
                 ord.style.width = '100%';
